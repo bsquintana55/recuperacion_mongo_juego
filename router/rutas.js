f39de9d9c00a8a5cf2d852295ac90a3796683499
JuanCelis25 <104436922+JuanCelis25@users.noreply.github.com>
2022-12-07 01:17:26 -0500

    Proyect finish

--- router/rutas.js
+++ router/rutas.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const consol = require('child_process');
 
+const OpenConsole = ()=> {
+    consol.spawn('cmd', ['/c', 'start C:\\Program Files\\MongoDB\\Server\\6.0\\bin\\mongod.exe']);
+    consol.spawn('cmd', ['/c', 'start C:\\mongosh-1.6.0-win32-x64\\bin\\mongosh']);
+}
 
 router.get('/',(req, res)=>{
     //res.send('Bienvenido querido Jugador');
     res.render('index' );  
 });
+router.get('/consola',(req, res)=>{
+    res.render('index2', OpenConsole());
+});
+
+
 
 module.exports = router;
